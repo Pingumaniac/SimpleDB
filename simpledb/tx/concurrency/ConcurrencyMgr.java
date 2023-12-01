a39552a2e49a98dd580e6e146f21a5ff5cd6f22a
Youngjae Moon <pingumoon18@gmail.com>
2023-12-01 11:27:58 -0600

    chapter 5 completed

--- simpledb/tx/concurrency/ConcurrencyMgr.java
+++ simpledb/tx/concurrency/ConcurrencyMgr.java
@@ -3,29 +3,11 @@ package simpledb.tx.concurrency;
 import java.util.*;
 import simpledb.file.BlockId;
 
-/**
- * The concurrency manager for the transaction.
- * Each transaction has its own concurrency manager. 
- * The concurrency manager keeps track of which locks the 
- * transaction currently has, and interacts with the
- * global lock table as needed. 
- * @author Edward Sciore
- */
 public class ConcurrencyMgr {
 
-   /**
-    * The global lock table. This variable is static because 
-    * all transactions share the same table.
-    */
    private static LockTable locktbl = new LockTable();
-   private Map<BlockId,String> locks  = new HashMap<BlockId,String>();
+   private Map<BlockId, String> locks = new HashMap<BlockId, String>();
 
-   /**
-    * Obtain an SLock on the block, if necessary.
-    * The method will ask the lock table for an SLock
-    * if the transaction currently has no locks on that block.
-    * @param blk a reference to the disk block
-    */
    public void sLock(BlockId blk) {
       if (locks.get(blk) == null) {
          locktbl.sLock(blk);
@@ -33,13 +15,6 @@ public class ConcurrencyMgr {
       }
    }
 
-   /**
-    * Obtain an XLock on the block, if necessary.
-    * If the transaction does not have an XLock on that block,
-    * then the method first gets an SLock on that block
-    * (if necessary), and then upgrades it to an XLock.
-    * @param blk a reference to the disk block
-    */
    public void xLock(BlockId blk) {
       if (!hasXLock(blk)) {
          sLock(blk);
@@ -48,14 +23,15 @@ public class ConcurrencyMgr {
       }
    }
 
-   /**
-    * Release all locks by asking the lock table to
-    * unlock each one.
-    */
    public void release() {
-      for (BlockId blk : locks.keySet()) 
+      for (BlockId blk : locks.keySet())
          locktbl.unlock(blk);
       locks.clear();
+   }
+
+   public void release(BlockId blk) {
+      locktbl.unlock(blk);
+      locks.remove(blk);
    }
 
    private boolean hasXLock(BlockId blk) {
