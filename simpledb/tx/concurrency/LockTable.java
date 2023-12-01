b5a7f7cb15f74c1b6c32834e41b70f5e4c8c7c2c
Youngjae Moon <pingumoon18@gmail.com>
2023-12-01 11:23:34 -0600

    update

--- simpledb/tx/concurrency/LockTable.java
+++ simpledb/tx/concurrency/LockTable.java
@@ -2,102 +2,130 @@ package simpledb.tx.concurrency;
 
 import java.util.*;
 import simpledb.file.BlockId;
+import simpledb.tx.Transaction;
 
-/**
- * The lock table, which provides methods to lock and unlock blocks.
- * If a transaction requests a lock that causes a conflict with an
- * existing lock, then that transaction is placed on a wait list.
- * There is only one wait list for all blocks.
- * When the last lock on a block is unlocked, then all transactions
- * are removed from the wait list and rescheduled.
- * If one of those transactions discovers that the lock it is waiting for
- * is still locked, it will place itself back on the wait list.
- * @author Edward Sciore
- */
 class LockTable {
    private static final long MAX_TIME = 10000; // 10 seconds
-   
-   private Map<BlockId,Integer> locks = new HashMap<BlockId,Integer>();
-   
-   /**
-    * Grant an SLock on the specified block.
-    * If an XLock exists when the method is called,
-    * then the calling thread will be placed on a wait list
-    * until the lock is released.
-    * If the thread remains on the wait list for a certain 
-    * amount of time (currently 10 seconds),
-    * then an exception is thrown.
-    * @param blk a reference to the disk block
-    */
-   public synchronized void sLock(BlockId blk) {
+   private Map<BlockId, Integer> locks = new HashMap<>();
+   private Map<BlockId, List<WaitingTransaction>> waitLists = new HashMap<>();
+   private Map<Integer, Long> transactionTimestamps = new HashMap<>();
+
+   public synchronized void sLock(BlockId blk, int txnum) {
       try {
          long timestamp = System.currentTimeMillis();
-         while (hasXlock(blk) && !waitingTooLong(timestamp))
+         transactionTimestamps.put(txnum, timestamp);
+
+         while (hasXlock(blk) && !waitingTooLong(timestamp)) {
+            if (waitDieLogic(txnum, blk)) { // Wait-Die logic
+               throw new LockAbortException();
+            }
+            WaitingTransaction wt = new WaitingTransaction(txnum, Thread.currentThread());
+            waitLists.computeIfAbsent(blk, k -> new ArrayList<>()).add(wt);
             wait(MAX_TIME);
+         }
          if (hasXlock(blk))
             throw new LockAbortException();
-         int val = getLockVal(blk);  // will not be negative
-         locks.put(blk, val+1);
-      }
-      catch(InterruptedException e) {
+         int val = getLockVal(blk);
+         locks.put(blk, val + 1);
+      } catch (InterruptedException e) {
          throw new LockAbortException();
       }
    }
-   
-   /**
-    * Grant an XLock on the specified block.
-    * If a lock of any type exists when the method is called,
-    * then the calling thread will be placed on a wait list
-    * until the locks are released.
-    * If the thread remains on the wait list for a certain 
-    * amount of time (currently 10 seconds),
-    * then an exception is thrown.
-    * @param blk a reference to the disk block
-    */
-   synchronized void xLock(BlockId blk) {
+
+   public synchronized void xLock(BlockId blk, int txnum) {
       try {
          long timestamp = System.currentTimeMillis();
-         while (hasOtherSLocks(blk) && !waitingTooLong(timestamp))
+         transactionTimestamps.put(txnum, timestamp);
+
+         while ((hasOtherSLocks(blk) || hasXlock(blk)) && !waitingTooLong(timestamp)) {
+            if (woundWaitLogic(txnum, blk)) { // Wound-Wait logic
+               throw new LockAbortException();
+            }
+            WaitingTransaction wt = new WaitingTransaction(txnum, Thread.currentThread());
+            waitLists.computeIfAbsent(blk, k -> new ArrayList<>()).add(wt);
             wait(MAX_TIME);
-         if (hasOtherSLocks(blk))
-            throw new LockAbortException();
+         }
          locks.put(blk, -1);
-      }
-      catch(InterruptedException e) {
+      } catch (InterruptedException e) {
          throw new LockAbortException();
       }
    }
-   
-   /**
-    * Release a lock on the specified block.
-    * If this lock is the last lock on that block,
-    * then the waiting transactions are notified.
-    * @param blk a reference to the disk block
-    */
-   synchronized void unlock(BlockId blk) {
+
+   public synchronized void unlock(BlockId blk, int txnum) {
       int val = getLockVal(blk);
       if (val > 1)
-         locks.put(blk, val-1);
+         locks.put(blk, val - 1);
       else {
          locks.remove(blk);
-         notifyAll();
+         notifyWaitingTransactions(blk);
       }
    }
-   
+
    private boolean hasXlock(BlockId blk) {
       return getLockVal(blk) < 0;
    }
-   
+
    private boolean hasOtherSLocks(BlockId blk) {
       return getLockVal(blk) > 1;
    }
-   
+
    private boolean waitingTooLong(long starttime) {
       return System.currentTimeMillis() - starttime > MAX_TIME;
    }
-   
+
    private int getLockVal(BlockId blk) {
-      Integer ival = locks.get(blk);
-      return (ival == null) ? 0 : ival.intValue();
+      Integer val = locks.get(blk);
+      return val == null ? 0 : val;
+   }
+
+   private void notifyWaitingTransactions(BlockId blk) {
+      List<WaitingTransaction> waitList = waitLists.get(blk);
+      if (waitList != null && !waitList.isEmpty()) {
+         WaitingTransaction wt = waitList.remove(0);
+         synchronized (wt.thread) {
+            wt.thread.notify();
+         }
+      }
+   }
+
+   private boolean waitDieLogic(int txnum, BlockId blk) {
+      Long txTimestamp = transactionTimestamps.get(txnum);
+      for (Map.Entry<Integer, Long> entry : transactionTimestamps.entrySet()) {
+         if (locks.containsKey(blk) && locks.get(blk) != -1 && txTimestamp != null) {
+            int otherTxNum = entry.getKey();
+            Long otherTxTimestamp = entry.getValue();
+            // If current transaction is older and waiting for a lock held by a younger
+            // transaction, it should abort.
+            if (otherTxNum != txnum && otherTxTimestamp != null && txTimestamp > otherTxTimestamp) {
+               return true;
+            }
+         }
+      }
+      return false;
+   }
+
+   private boolean woundWaitLogic(int txnum, BlockId blk) {
+      Long txTimestamp = transactionTimestamps.get(txnum);
+      for (Map.Entry<Integer, Long> entry : transactionTimestamps.entrySet()) {
+         if (locks.containsKey(blk) && locks.get(blk) != -1 && txTimestamp != null) {
+            int otherTxNum = entry.getKey();
+            Long otherTxTimestamp = entry.getValue();
+            // If current transaction is older and a younger transaction holds the lock,
+            // wound the younger transaction.
+            if (otherTxNum != txnum && otherTxTimestamp != null && txTimestamp < otherTxTimestamp) {
+               return true;
+            }
+         }
+      }
+      return false;
+   }
+   private static class WaitingTransaction {
+      int txnum;
+      Thread thread;
+
+      WaitingTransaction(int txnum, Thread thread) {
+         this.txnum = txnum;
+         this.thread = thread;
+      }
    }
 }
