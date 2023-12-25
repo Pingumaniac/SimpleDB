57346792e35143fb03acf79a7c4c3948b4769276
Youngjae Moon <pingumoon18@gmail.com>
2023-12-25 13:59:16 -0600

    update

--- simpledb/plan/Planner.java
+++ simpledb/plan/Planner.java
@@ -2,68 +2,113 @@ package simpledb.plan;
 
 import simpledb.tx.Transaction;
 import simpledb.parse.*;
+import simpledb.metadata.MetadataMgr;
 
-/**
- * The object that executes SQL statements.
- * @author Edward Sciore
- */
 public class Planner {
    private QueryPlanner qplanner;
    private UpdatePlanner uplanner;
-   
+
    public Planner(QueryPlanner qplanner, UpdatePlanner uplanner) {
       this.qplanner = qplanner;
       this.uplanner = uplanner;
    }
-   
-   /**
-    * Creates a plan for an SQL select statement, using the supplied planner.
-    * @param qry the SQL query string
-    * @param tx the transaction
-    * @return the scan corresponding to the query plan
-    */
+
    public Plan createQueryPlan(String qry, Transaction tx) {
       Parser parser = new Parser(qry);
       QueryData data = parser.query();
       verifyQuery(data);
       return qplanner.createPlan(data, tx);
    }
-   
-   /**
-    * Executes an SQL insert, delete, modify, or
-    * create statement.
-    * The method dispatches to the appropriate method of the
-    * supplied update planner,
-    * depending on what the parser returns.
-    * @param cmd the SQL update string
-    * @param tx the transaction
-    * @return an integer denoting the number of affected records
-    */
+
    public int executeUpdate(String cmd, Transaction tx) {
       Parser parser = new Parser(cmd);
       Object data = parser.updateCmd();
       verifyUpdate(data);
       if (data instanceof InsertData)
-         return uplanner.executeInsert((InsertData)data, tx);
+         return uplanner.executeInsert((InsertData) data, tx);
       else if (data instanceof DeleteData)
-         return uplanner.executeDelete((DeleteData)data, tx);
+         return uplanner.executeDelete((DeleteData) data, tx);
       else if (data instanceof ModifyData)
-         return uplanner.executeModify((ModifyData)data, tx);
+         return uplanner.executeModify((ModifyData) data, tx);
       else if (data instanceof CreateTableData)
-         return uplanner.executeCreateTable((CreateTableData)data, tx);
+         return uplanner.executeCreateTable((CreateTableData) data, tx);
       else if (data instanceof CreateViewData)
-         return uplanner.executeCreateView((CreateViewData)data, tx);
+         return uplanner.executeCreateView((CreateViewData) data, tx);
       else if (data instanceof CreateIndexData)
-         return uplanner.executeCreateIndex((CreateIndexData)data, tx);
+         return uplanner.executeCreateIndex((CreateIndexData) data, tx);
       else
          return 0;
    }
- 
-   // SimpleDB does not verify queries, although it should.
+
    private void verifyQuery(QueryData data) {
+      for (String tblName : data.tables()) {
+         if (!tableExists(tblName)) {
+            throw new NonexistentTableException(tblName);
+         }
+      }
+
+      for (String fieldName : data.fields()) {
+         if (!fieldExists(fieldName)) {
+            throw new NonexistentFieldException(fieldName);
+         }
+         if (isFieldAmbiguous(fieldName, data.tables())) {
+            throw new AmbiguousFieldException(fieldName);
+         }
+      }
    }
 
-   // SimpleDB does not verify updates, although it should.
    private void verifyUpdate(Object data) {
+      if (data instanceof InsertData) {
+         InsertData insertData = (InsertData) data;
+         if (!tableExists(insertData.tableName())) {
+            throw new NonexistentTableException(insertData.tableName());
+         }
+      } else if (data instanceof DeleteData) {
+         DeleteData deleteData = (DeleteData) data;
+         if (!tableExists(deleteData.tableName())) {
+            throw new NonexistentTableException(deleteData.tableName());
+         }
+      } else if (data instanceof ModifyData) {
+         ModifyData modifyData = (ModifyData) data;
+         if (!tableExists(modifyData.tableName())) {
+            throw new NonexistentTableException(modifyData.tableName());
+         }
+      }
+   }
+
+   private boolean tableExists(String tableName) {
+      return MetadataMgr.tableExists(tableName);
+   }
+
+   private boolean fieldExists(String fieldName) {
+      return MetadataMgr.fieldExists(fieldName);
+   }
+
+   private boolean isFieldAmbiguous(String fieldName, List<String> tables) {
+      int count = 0;
+      for (String table : tables) {
+         if (MetadataMgr.fieldExistsInTable(fieldName, table)) {
+            count++;
+         }
+      }
+      return count > 1;
+   }
+}
+
+class NonexistentTableException extends RuntimeException {
+   public NonexistentTableException(String tableName) {
+      super("Table '" + tableName + "' does not exist.");
+   }
+}
+
+class NonexistentFieldException extends RuntimeException {
+   public NonexistentFieldException(String fieldName) {
+      super("Field '" + fieldName + "' does not exist.");
+   }
+}
+
+class AmbiguousFieldException extends RuntimeException {
+   public AmbiguousFieldException(String fieldName) {
+      super("Field name '" + fieldName + "' is ambiguous.");
    }
 }
