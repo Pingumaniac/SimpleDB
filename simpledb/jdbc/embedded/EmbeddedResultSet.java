2dacaec582fbad0f5bf3bb90157fffd2f9966cdd
Youngjae Moon <pingumoon18@gmail.com>
2023-12-31 10:21:36 -0600

    jdbc update

--- simpledb/jdbc/embedded/EmbeddedResultSet.java
+++ simpledb/jdbc/embedded/EmbeddedResultSet.java
@@ -6,31 +6,17 @@ import simpledb.query.Scan;
 import simpledb.plan.Plan;
 import simpledb.jdbc.ResultSetAdapter;
 
-/**
- * The embedded implementation of ResultSet.
- * @author Edward Sciore
- */
 public class EmbeddedResultSet extends ResultSetAdapter {
    private Scan s;
    private Schema sch;
    private EmbeddedConnection conn;
 
-   /**
-    * Creates a Scan object from the specified plan.
-    * @param plan the query plan
-    * @param conn the connection
-    * @throws RemoteException
-    */
    public EmbeddedResultSet(Plan plan, EmbeddedConnection conn) throws SQLException {
       s = plan.open();
       sch = plan.schema();
       this.conn = conn;
    }
 
-   /**
-    * Moves to the next record in the result set,
-    * by moving to the next record in the saved scan.
-    */
    public boolean next() throws SQLException {
       try {
          return s.next();
@@ -41,10 +27,6 @@ public class EmbeddedResultSet extends ResultSetAdapter {
       }
    }
 
-   /**
-    * Returns the integer value of the specified field,
-    * by returning the corresponding value on the saved scan.
-    */
    public int getInt(String fldname) throws SQLException {
       try {
          fldname = fldname.toLowerCase(); // to ensure case-insensitivity
@@ -56,10 +38,6 @@ public class EmbeddedResultSet extends ResultSetAdapter {
       }
    }
 
-   /**
-    * Returns the integer value of the specified field,
-    * by returning the corresponding value on the saved scan.
-    */
    public String getString(String fldname) throws SQLException {
       try {
          fldname = fldname.toLowerCase(); // to ensure case-insensitivity
@@ -71,20 +49,29 @@ public class EmbeddedResultSet extends ResultSetAdapter {
       }
    }
 
-   /**
-    * Returns the result set's metadata,
-    * by passing its schema into the EmbeddedMetaData constructor.
-    */
    public ResultSetMetaData getMetaData() throws SQLException {
       return new EmbeddedMetaData(sch);
    }
 
-   /**
-    * Closes the result set by closing its scan, and commits.
-    */
    public void close() throws SQLException {
       s.close();
       conn.commit();
+   }
+
+   @Override
+   public void beforeFirst() throws SQLException {
+      s.beforeFirst();
+   }
+
+   @Override
+   public boolean absolute(int n) throws SQLException {
+      s.beforeFirst();
+      for (int i = 0; i < n; i++) {
+         if (!s.next()) {
+            return false;
+         }
+      }
+      return true;
    }
 }
 
--- simpledb/jdbc/network/RemoteResultSetImpl.java
+++ simpledb/jdbc/network/RemoteResultSetImpl.java
@@ -7,34 +7,18 @@ import simpledb.record.Schema;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-/**
- * The RMI server-side implementation of RemoteResultSet.
- * @author Edward Sciore
- */
 @SuppressWarnings("serial")
 class RemoteResultSetImpl extends UnicastRemoteObject implements RemoteResultSet {
    private Scan s;
    private Schema sch;
    private RemoteConnectionImpl rconn;
 
-   /**
-    * Creates a RemoteResultSet object.
-    * The specified plan is opened, and the scan is saved.
-    * @param plan the query plan
-    * @param rconn TODO
-    * @throws RemoteException
-    */
    public RemoteResultSetImpl(Plan plan, RemoteConnectionImpl rconn) throws RemoteException {
       s = plan.open();
       sch = plan.schema();
       this.rconn = rconn;
    }
 
-   /**
-    * Moves to the next record in the result set,
-    * by moving to the next record in the saved scan.
-    * @see simpledb.jdbc.network.RemoteResultSet#next()
-    */
    public boolean next() throws RemoteException {
 		try {
 	      return s.next();
@@ -45,11 +29,6 @@ class RemoteResultSetImpl extends UnicastRemoteObject implements RemoteResultSet
       }
    }
 
-   /**
-    * Returns the integer value of the specified field,
-    * by returning the corresponding value on the saved scan.
-    * @see simpledb.jdbc.network.RemoteResultSet#getInt(java.lang.String)
-    */
    public int getInt(String fldname) throws RemoteException {
 		try {
 	      fldname = fldname.toLowerCase(); // to ensure case-insensitivity
@@ -61,11 +40,6 @@ class RemoteResultSetImpl extends UnicastRemoteObject implements RemoteResultSet
       }
    }
 
-   /**
-    * Returns the integer value of the specified field,
-    * by returning the corresponding value on the saved scan.
-    * @see simpledb.jdbc.network.RemoteResultSet#getInt(java.lang.String)
-    */
    public String getString(String fldname) throws RemoteException {
 		try {
 	      fldname = fldname.toLowerCase(); // to ensure case-insensitivity
@@ -77,22 +51,29 @@ class RemoteResultSetImpl extends UnicastRemoteObject implements RemoteResultSet
       }
    }
 
-   /**
-    * Returns the result set's metadata,
-    * by passing its schema into the RemoteMetaData constructor.
-    * @see simpledb.jdbc.network.RemoteResultSet#getMetaData()
-    */
    public RemoteMetaData getMetaData() throws RemoteException {
       return new RemoteMetaDataImpl(sch);
    }
 
-   /**
-    * Closes the result set by closing its scan.
-    * @see simpledb.jdbc.network.RemoteResultSet#close()
-    */
    public void close() throws RemoteException {
       s.close();
       rconn.commit();
+   }
+
+   @Override
+   public void beforeFirst() throws RemoteException {
+      s.beforeFirst();
+   }
+
+   @Override
+   public boolean absolute(int n) throws RemoteException {
+      s.beforeFirst();
+      for (int i = 0; i < n; i++) {
+         if (!s.next()) {
+            return false;
+         }
+      }
+      return true;
    }
 }
 
